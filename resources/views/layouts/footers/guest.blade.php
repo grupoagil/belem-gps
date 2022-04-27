7cafe132ca083ce613272ffb551ddf5c564e65dd
Daniel Pinon <danielpinon@danielpinon.com.br>
2022-04-27 10:09:37 -0300

    Feat: Git Init

--- resources/views/layouts/footers/guest.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container">
-        <nav class="float-left">
-        </nav>
-        <div class="copyright float-center">
-        &reg; Feito por Ágil Desenvolvimento de Sistema</a>. {{date('Y')}}.
-        </div>
-    </div>
-</footer> 
