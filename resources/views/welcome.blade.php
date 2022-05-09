ca94a4ee3e930de333769ee90763089795c8f4fa
Daniel Pinon <danielpinon@danielpinon.com.br>
2022-05-09 13:15:04 -0300

    Feat: Ajustes do site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,10 @@
                                         data-cielo="{{$item['PROD_LINK_CIELO']}}"
                                         data-magalu="{{$item['PROD_LINK_MAGALU']}}"
                                         data-descricao="{{$item['PROD_DESCRICAO']}}"
-                                        data-desconto="{{$item['PROD_DESCONTO_CIELO']}}">
+                                        data-desconto="{{$item['PROD_DESCONTO_CIELO']}}"
+                                        data-imagem="{{asset(str_replace('public','storage',$item['PROD_IMAGE']))}}"
+                                        data-titulo="{{$item['PROD_NOME']}}"
+                                        data-toggle="modal" data-target="#produtoPreview">
                                         <div class="row justify-content-between align-items-center">
                                             @if ($item['PROD_DESCONTO_CIELO'] != 0)
                                                 <div class="percent">-{{$item['PROD_DESCONTO_CIELO']}}%</div>
@@ -130,7 +133,10 @@
                                     data-cielo="{{$item['PROD_LINK_CIELO']}}"
                                     data-magalu="{{$item['PROD_LINK_MAGALU']}}"
                                     data-descricao="{{$item['PROD_DESCRICAO']}}"
-                                    data-desconto="{{$item['PROD_DESCONTO_CIELO']}}">
+                                    data-desconto="{{$item['PROD_DESCONTO_CIELO']}}"
+                                    data-imagem="{{asset(str_replace('public','storage',$item['PROD_IMAGE']))}}"
+                                    data-titulo="{{$item['PROD_NOME']}}"
+                                    data-toggle="modal" data-target="#produtoPreview">
                                     <div class="row justify-content-between align-items-center">
                                         @if ($item['PROD_DESCONTO_CIELO'] != 0)
                                             <div class="percent">-{{$item['PROD_DESCONTO_CIELO']}}%</div>
@@ -172,6 +178,36 @@
           </div>
       </section>
   </main>
+  <!-- Modal Preview Produto -->
+    <div class="modal fade" id="produtoPreview" tabindex="-1" role="dialog" aria-labelledby="produtoPreviewLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="produtoPreviewLabel">Preview Produto</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-6">
+                        <img src="" alt="Imagem do Produto" width="100%">
+                    </div>
+                    <div class="col-6">
+                        <div class="" id="descricao">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Cielo</button>
+                <button type="button" class="btn btn-danger">Magalu</button>
+            </div>
+        </div>
+        </div>
+    </div>
 @endsection
 
 @push('js')
@@ -195,6 +231,20 @@
                     window.location.href = magalu;
                 }
             })
+        })
+    </script>
+    <script>
+        $('#produtoPreview').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget) // Button that triggered the modal
+            var imagem = button.data('imagem') // Extract info from data-* attributes
+            var titulo = button.data('titulo')
+            var descricao = button.data('descricao')
+            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+            var modal = $(this)
+            modal.find('.modal-title').text(titulo)
+            modal.find('img').attr('src',imagem)
+            modal.find('#descricao').text(descricao);
         })
     </script>
 @endpush
