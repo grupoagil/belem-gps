435fc7b9079005c20f632774376c568dfd1b7d02
Daniel Pinon <danielpinon@danielpinon.com.br>
2022-05-27 16:05:21 -0300

    Feat: Ajustes da lista de serviços

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,12 +45,12 @@
                       {{-- <div class="main-content">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
                       <small>Lorem Ipsum</small> --}}
                       <nav class="list-unstyled">
-                          <li>• Análise de Plano de Manejo Florestal</li>
-                          <li>• Revendedor de Drones DJI</li>
-                          <li>• Laudos Técnicos de Cobertura e Uso da Terra</li>
-                          <li>• Regularização Fundiária e Ambiental</li>
-                          <li>• Ativação de CAR e Aprovação de Reserva Legal</li>
-                          <li>• Desembargos no IBAMA, ICMbio e Secretarias</li>
+                          <li><a href="#" data-toggle="modal" data-target="#servicoTexto" data-titulo="Análise de Plano de Manejo Florestal" data-texto="Manejo florestal consiste na exploração de madeira de forma seletiva com base no inventário florestal, a análise do plano tem por objetivo verificar e monitorar os dados da exploração e reportar possíveis inconsistência dos dados para que não afete o volume licenciado, assim como o monitoramento da cobertura florestal. Realizamos também levantamentos, de com uso de ferramentas de geoprocessamento, de áreas potenciais e operacionais para o planejamento de PMFS com análise temporal da cobertura do solo.">Análise de Plano de Manejo Florestal</a></li>
+                          <li><a href="#" data-toggle="modal" data-target="#servicoTexto" data-titulo="Revendedor de Drones DJI" data-texto="Dispomos de drones DJI a pronta entrega. Especialmente os modelos da família Mavic, como Mavic Mini SE e Mavic Mini 2. São drones com peso de decolagem abaixo de 250g, duração média de voo de 30 minutos e que possuem gimbal de três eixos. A vantagem desses modelos mais leves, é justamente o tamanho pequeno, que facilita levar até mesmo na mochila. Esse tipo de drone atende a diversos públicos, desde pessoas que trabalham com fotografia e produção de vídeos, a trabalhos de engenharia, construção civil, inspeção de obras, inspeção ambiental, entre outros.">Revendedor de Drones DJI</a></li>
+                          <li><a href="#" data-toggle="modal" data-target="#servicoTexto" data-titulo="Laudos Técnicos de Cobertura e Uso da Terra" data-texto="Tais laudos consistem em revisar a classificação da cobertura vegetal de imóveis rurais cadastrados ou não no CAR, podendo ser aplicado em diversas situações, como a análise diacrônica de imagens de satélite a fim de desqualificar um polígono do PRODES, comprovar área consolidada existente antes de 22 de julho de 2008, comprovar o uso recorrente de vegetação em estágio inicial de regeneração, para delimitar áreas passíveis de comunicação de limpeza de capoeira, entre outros. Estes laudos possuem arcabouço teórico, técnico e jurídico.">Laudos Técnicos de Cobertura e Uso da Terra</a></li>
+                          <li><a href="#" data-toggle="modal" data-target="#servicoTexto" data-titulo="Regularização Fundiária e Ambiental" data-texto="Os serviços de regularização fundiária do imóvel rural garantem o cumprimento das obrigações cadastrais, jurídicas e tributarias, sendo necessária a inscrição para formalizar as operações de compra e vendas de novas terras, bem como para obtenção de financiamentos rurais, empréstimos, processos ambientais e licenciamentos para atividade rural. O imóvel rural precisa estar regulamentado no Sistema Nacional de Cadastro Rural – SNCR, do Instituto Nacional de Colonização e Reforma Agrária – INCRA para aquisição do certificado indispensável para que possa ser arrendada, vendida, hipotecada ou desmembrada, uma parcela ou toda, propriedade rural. Assim como, estar regulamentado no âmbito tributário, sendo necessário declarar anualmente o Imposto sobre a Propriedade Rural – ITR. Para o imóvel estar regularizado ambientalmente, são necessárias as etapas envolvendo o trabalho de mapear a área, executar o georreferenciamento, fazer a inscrição no Cadastro Ambiental Rural – CAR, e delimitar e avaliar o carecimento de recomposição ou compensação de Reserva Legal, tal como de Áreas de Preservação Permanente, para posteriormente realizar o devido cumprimento no Programa de Regularização Ambiental – PRA. Além disso, é necessário o cumprimento das obrigações estabelecidas no Termo de Compromisso Ambiental firmado junto a Secretaria Estadual de Meio Ambiente, sendo específico para cada imóvel.">Regularização Fundiária e Ambiental</a></li>
+                          <li><a href="#" data-toggle="modal" data-target="#servicoTexto" data-titulo="Ativação de CAR e Aprovação de Reserva Legal" data-texto="O Cadastro Ambiental Rural foi criado junto com o Código Florestal com intuito de promover a transparência das propriedades e consequentemente sobre as áreas brasileiras. Adequa-se como planejamento econômico e ambiental para essas propriedades cadastradas, obrigando os proprietários rurais a fazerem a regularização ambiental da propriedade rural. O processo de ativação do CAR é feito com base em estudo multitemporal de imagens de satélites sobre a área do ímovel, para que seja feita a correta classificação da cobertura e uso da terra e delimitação das Áreas Consolidadas, Remanescente de Vegetação Nativa, Área de Proteção Permanente, assim como outras classes. A Reserva Legal é delimitada em áreas de Remanescente de Vegetação Nativa, além de áreas em que o proprietário será obrigado a recompor floresta, como em áreas desmatadas pós 22 de julho de 2008.">Ativação de CAR e Aprovação de Reserva Legal</a></li>
+                          <li><a href="#" data-toggle="modal" data-target="#servicoTexto" data-titulo="Desembargos no IBAMA, ICMbio e Secretarias" data-texto="Os desembargos junto aos órgãos fiscalizadores ocorrem por meio de relatório técnico que comprove, a partir de imagens de satélite do imóvel rural, que a área embargada já se encontrava como Área Consolidada antes de 22 de julho de 2008, o marco regulatório do Código Florestal. O imóvel qualificado para o desembargo necessita ter o Recibo de Adesão ao CAR.">Desembargos no IBAMA, ICMbio e Secretarias</a></li>
                           <a href="{{route('servicos')}}" class="arrow">Saiba mais</a>
                       </nav>
                   </div>
@@ -175,8 +175,8 @@
               </div>
           </div>
       </section>
-  </main>
-  <!-- Modal Preview Produto -->
+    </main>
+    <!-- Modal Preview Produto -->
     <div class="modal fade" id="produtoPreview" tabindex="-1" role="dialog" aria-labelledby="produtoPreviewLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
@@ -206,6 +206,25 @@
         </div>
         </div>
     </div>
+    <!-- Modal -->
+    <div class="modal fade" id="servicoTexto" tabindex="-1" role="dialog" aria-labelledby="servicoTextoLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="servicoTextoLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('js')
@@ -225,6 +244,18 @@
             modal.find('#descricao').text(descricao);
             modal.find('.btn-primary').attr('href',cielo)
             modal.find('.btn-danger').attr('href',magalu)
+        })
+    </script>
+    <script>
+        $('#servicoTexto').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget) // Button that triggered the modal
+            var titulo = button.data('titulo') // Extract info from data-* attributes
+            var texto = button.data('texto') // Extract info from data-* attributes
+            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+            var modal = $(this)
+            modal.find('.modal-title').text(titulo)
+            modal.find('.modal-body').html(texto)
         })
     </script>
 @endpush
