353278651ac9470baebef03eb08d7d6e186b5383
danielpinon <danielpinon@danielpinon.com.br>
2022-11-11 13:10:08 -0300

    Fix: Multi redirecionamento

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,10 +16,6 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure()) {
-            return redirect()->secure($request->getRequestUri());
-        }
-
         return $next($request);
     }
 }
