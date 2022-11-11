94ed00f399bb22d4d208a0361273def2c729e0d2
belemgp2 <contato@agildesenvolvimento.com>
2022-11-11 13:01:34 -0300

    Fix: Correção de bug ao redirecionar para pasta public

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if(config('app.url') != "http://localhost"){
+            URL::forceScheme('https');
+        }
     }
 }
